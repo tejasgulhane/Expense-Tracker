0345406fd676d1ccd7d1b32125accb49965f3086
tejasgulhane <96588203+tejasgulhane@users.noreply.github.com>
2022-08-22 13:01:05 +0530

    M6-Task1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import HomePage from './components/pages/HomePage';
+import './style.css';
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Homepage.css'
+import Form from './Form'
+
+const HomePage = () => {
+  return (
+    <Form />
+  )
+}
+
+export default HomePage
